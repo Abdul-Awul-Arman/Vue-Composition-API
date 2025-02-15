27023aa2fc65524ae10c7af67ac8e3cfb76bc04d
Abdul-Awul-Arman <abdulawularman52114@gmail.com>
2025-02-15 14:02:31 +0600

    first commit

---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props=defineProps({
+  name:{
+    type:String,
+    required:true,
+  }
+})
+</script>
+<template>
+  <div class="plan">
+      <div class="description">
+        <span class="title">vvvv </span>
+      </div>
+    </div>
+</template>
